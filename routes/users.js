c1f9ef9394d75962ffebe9373bd770bbc504c7a3
Toshihiro Inao <t.inao@0xtech.io>
2018-09-30 11:31:18 +0900

    login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/login/', function(req, res, next) {
+  res.render('users/login', { title: 'ログイン' })
+});
+
+router.get('/logout/', function(req, res, next) {
+  res.render('users/logout', { title: 'ログアウト' })
 });
 
 module.exports = router;
