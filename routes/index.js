55ac0f43732f9078169ffe5c74a4e3c54620be3c
hirash22 <hirash22@hotmail.com>
2018-09-30 12:17:08 +0900

    add withdraw

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
-const { paymentMonitor } = require("../lib/payment_monitor");
+const { sendPayment } = require("../lib/send_payment");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('./items/');
 });
 
-router.get('/check', function(req, res, next) {
-  paymentMonitor();
+router.get('/refund', function(req, res, next) {
+  res.render('refund/index', { title: '返金' })
+});
+
+router.get('/refund/send', function(req, res, next) {
+  sendPayment(req.query.payreq);
+  res.render('refund/thanks', { title: "処理完了" })
 });
 
 module.exports = router;
